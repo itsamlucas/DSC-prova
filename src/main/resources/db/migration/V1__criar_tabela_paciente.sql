837c79316ce42d9fafbd8171fd10989756b8d888
Samuel Lucas <115077019+itsamlucas@users.noreply.github.com>
2024-01-03 11:01:17 -0300

    Update V1__criar_tabela_paciente.sql

--- src/main/resources/db/migration/V1__criar_tabela_paciente.sql
+++ src/main/resources/db/migration/V1__criar_tabela_paciente.sql
@@ -1,7 +1,6 @@
 CREATE TABLE Paciente (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     cpf VARCHAR(14) NOT NULL,
     historico_medico TEXT,
-    UNIQUE (cpf)
 );
