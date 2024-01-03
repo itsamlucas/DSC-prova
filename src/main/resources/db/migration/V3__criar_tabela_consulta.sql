500146edf0fbbb928d7964e3953fb6469b3d3d2d
Samuel Lucas <115077019+itsamlucas@users.noreply.github.com>
2024-01-03 11:01:53 -0300

    Update V3__criar_tabela_consulta.sql

--- src/main/resources/db/migration/V3__criar_tabela_consulta.sql
+++ src/main/resources/db/migration/V3__criar_tabela_consulta.sql
@@ -1,10 +1,10 @@
 CREATE TABLE Consulta (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     data_consulta TIMESTAMP NOT NULL,
     diagnostico TEXT,
     prescricao TEXT,
-    paciente_id BIGINT,
-    medico_id BIGINT,
+    paciente_id INT,
+    medico_id INT,
     FOREIGN KEY (paciente_id) REFERENCES Paciente(id),
     FOREIGN KEY (medico_id) REFERENCES Médico(id)
 );
